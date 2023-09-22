159f7c1c4e3fc0e647e1c3218d5db19e6f020c7f
RuslanFor <rusik186rusik@yandex.ru>
2023-09-22 23:11:19 +0500

    dddddвh

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin:['http://localhost:3001']
+    origin:['http://localhost:3001','https://shop-frontik-9zkapv8ho-ruslanfor.vercel.app']
   })
   
   const config = new DocumentBuilder()
